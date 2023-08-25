b78fa3abc34a0b2b39349346e155d1673a3f1085
Caique Zaneti Kirilo <caiquez.kirilo@hotmail.com>
2023-08-24 21:02:18 -0300

    Final da Aula

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Runtime.ExceptionServices;
+using Plantao_UC_COD_BACK.Classes;
+
+string opcao = "";
+PessoaJuridica PJ = new PessoaJuridica();
+
+do{
+    Console.WriteLine(@"
+    
+    1 - Cadastrar
+    2 - Sair
+    
+    ");
+
+    opcao = Console.ReadLine();
+
+    if (opcao == "1"){
+        PessoaJuridica pj = new PessoaJuridica();
+        Console.WriteLine("Digite o nome da Pessoa Juridica");
+        pj.Nome = Console.ReadLine();
+
+        Console.WriteLine("Digite o rendimento da Pessoa Juridica");
+        pj.Rendimento =  float.Parse(Console.ReadLine());
+        
+        Console.WriteLine("Digite o cnpj da Pessoa Juridica");
+        pj.Cnpj = Console.ReadLine();
+
+        PJ.Inserir(pj);
+    }
+}while(opcao != "2");
+
+
